77e9e6d48b8b7c467c1363948f694e36ca0f247b
saathwik-tk <77228369+saathwik-tk@users.noreply.github.com>
2025-02-09 19:26:11 +0530

    Implemented functions

--- cost_calculator.py
+++ cost_calculator.py
@@ -19,28 +19,38 @@ the cost will be printed whenever a 2nd command is encountered.
 
 q = int(input())
 commands = []
+
+max_element = 0
 for i in range(q):
   commands.append(list(map(int,input().split(" "))))
+  max_element = max(max_element, commands[i][1], commands[i][2])
   
-def shortest_path(graph):
+def get_shortest_path(graph):
   path_arr=[]
   return path_arr
 
-def create_graph():
+def create_graph(n):
   g={}
   return g
 
 def update_cost(graph, start, end, cost):
-  return
+  shortest_path = get_shortest_path(graph, start, end)
+  for i in range(1,len(shortest_path)):
+    graph[shortest_path[i-1]][shortest_path[i]] += cost
+    graph[shortest_path[i]][shortest_path[i-1]] += cost
+  return graph
 
 def get_current_cost(graph, start, end):
   current_cost=0
+  shortest_path = get_shortest_path(graph, start, end)
+  for i in range(1,len(shortest_path)):
+    current_cost += graph[shortest_path[i-1]][shortest_path[i]]
   return current_cost
 
-g=create_graph()
+g = create_graph(max_element)
 for command in commands:
   if command[0]==1:
-    update_cost(g, command[1], command[2], command[3])
+    g = update_cost(g, command[1], command[2], command[3])
   else:
     print(get_current_cost(g, command[1], command[2]))
     
