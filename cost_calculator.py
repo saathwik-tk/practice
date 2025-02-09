9fda93995d1d07ddfaabf926d6a8479dddc2e816
saathwik-tk <77228369+saathwik-tk@users.noreply.github.com>
2025-02-09 20:26:28 +0530

    Completed the solution

--- cost_calculator.py
+++ cost_calculator.py
@@ -19,30 +19,71 @@ the cost will be printed whenever a 2nd command is encountered.
 
 q = int(input())
 commands = []
-
+from collections import deque
+ 
 max_element = 0
 for i in range(q):
   commands.append(list(map(int,input().split(" "))))
   max_element = max(max_element, commands[i][1], commands[i][2])
   
-def get_shortest_path(graph):
-  path_arr=[]
-  return path_arr
+def get_shortest_path(graph,start,end):
+  parent_map = {}  # {child_val: parent_val}
+  queue = deque([1])
+
+  while queue:
+      node = queue.popleft()
+      if 2*node in graph:
+          parent_map[2*node] = node
+          queue.append(2*node)    
+      if 2*node+1 in graph:
+          parent_map[2*node+1] = node
+          queue.append(2*node+1)
+
+  ancestors = set()
+  current_start = start
+  while current_start in parent_map:
+      ancestors.add(current_start)
+      current_start = parent_map[current_start]
+  ancestors.add(current_start)
+
+  current_end = end
+  while current_end not in ancestors:
+      current_end = parent_map[current_end]
+
+  lca = current_end
+
+  path_start = []
+  current_start = start
+  while current_start != lca:
+      path_start.append(current_start)
+      current_start = parent_map[current_start]
+  path_start.append(lca)
+
+  path_end = []
+  current_end = end 
+  while current_end != lca:
+      path_end.append(current_end)
+      current_end = parent_map[current_end]
+
+  return path_start + path_end[::-1]
 
 create_graph = lambda x: {i: {2*i:0, 2*i+1:0} for i in range(1,x+1)}
 
 def update_cost(graph, start, end, cost):
   shortest_path = get_shortest_path(graph, start, end)
   for i in range(1,len(shortest_path)):
-    graph[shortest_path[i-1]][shortest_path[i]] += cost
-    graph[shortest_path[i]][shortest_path[i-1]] += cost
+    min_ele = min(shortest_path[i-1],shortest_path[i])
+    max_ele = max(shortest_path[i-1],shortest_path[i])
+    graph[min_ele][max_ele] += cost
   return graph
 
 def get_current_cost(graph, start, end):
   current_cost=0
   shortest_path = get_shortest_path(graph, start, end)
   for i in range(1,len(shortest_path)):
-    current_cost += graph[shortest_path[i-1]][shortest_path[i]]
+    min_ele = min(shortest_path[i-1],shortest_path[i])
+    max_ele = max(shortest_path[i-1],shortest_path[i])
+    current_cost += graph[min_ele][max_ele]
   return current_cost
 
 g = create_graph(max_element)
